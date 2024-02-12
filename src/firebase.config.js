f77e85cac6fe25d56a0a28c33f319d8da6c56c23
Vladyslav <merkard@Vladyslavs-MacBook-Pro-2.local>
2024-02-11 17:10:40 -0700

    fixed apiKeys

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_CONFIG_API_KEY,
+  apiKey: process.env.REACT_APP_CONFIG_API_KEY,
   authDomain: "swift-homes-app.firebaseapp.com",
   projectId: "swift-homes-app",
   storageBucket: "swift-homes-app.appspot.com",
-  messagingSenderId: process.env.FIREBASE_CONFIG_SENDER_ID,
-  appId: process.env.FIREBASE_CONFIG_APP_ID,
+  messagingSenderId: process.env.REACT_APP_CONFIG_SENDER_ID,
+  appId: process.env.REACT_APP_CONFIG_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
